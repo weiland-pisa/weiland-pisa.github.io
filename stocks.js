05584fd916c92790823c4748a15c7b4a04bebebe
Ronald Weiland <weiland@pisasales.de>
2021-04-29 16:33:49 +0200

    layout updates

---
+++ stocks.js
@@ -0,0 +1,21 @@
+function insertAllLinks() {
+	insertLinks(etfs, 'etfs');
+	insertLinks(stocks, 'stocks');
+}
+
+function insertLinks(json, tag) {
+	//var json = JSON.parse(data);
+	for (var idx=json.length-1; idx>=0; idx--){
+		var head = document.getElementById(tag);
+		var link = document.createElement('a');
+		link.setAttribute('href', json[idx].url);
+		link.setAttribute('target', '_blank');
+		link.setAttribute('class', 'button');
+		link.innerHTML = json[idx].name;
+		head.parentNode.insertBefore(link, head.nextSibling);
+		var lbr = document.createElement('br');
+		link.parentNode.insertBefore(lbr, link.nextSibling);
+	}
+}
+
+window.onload = insertAllLinks;
